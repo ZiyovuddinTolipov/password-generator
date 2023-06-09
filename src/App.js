47f8634b07e506e56ae72f5a9c868530a960c79c
tolipov <ziyovuddin.developer@gmail.com>
2023-06-09 05:28:52 +0500

    structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,46 @@ import './scss/App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Password Generator App By Tolipov</h1>
+      <div className='container'>
+        <div className='generator'>
+          <h2 className='generator__header'>
+            Password Generator
+          </h2>
+          <div className='generator__password'>
+            <h3> Password </h3>
+            <button className='copy__btn'>
+              <i className='far fa-clipboard'></i>
+            </button>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password-strenght'>Password Strenght</label>
+            <input
+              type="number"
+              id='password-strenght'
+              name='password-strenght'
+              max="20"
+              min="10" />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='uppercase-letters'>Include Uppercase Letters</label>
+            <input
+              type="checkbox"
+              id='uppercase-letters'
+              name='uppercase-letters'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='lowercase-letters'>Include Lowercase Letters</label>
+            <input
+              type="checkbox"
+              id='lowercase-letters'
+              name='lowercase-letters'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
