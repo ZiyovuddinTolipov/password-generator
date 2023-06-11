cb7a91ea755d91a83b592e17fdb90b77ebd8e186
tolipov <ziyovuddin.developer@gmail.com>
2023-06-11 10:14:37 +0500

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import './scss/App.scss';
 
 function App() {
+  const [password,setPassword] = useState('');
+  const [passwordlenght,setPasswordlenght] = useState(20);
+  const [includeUppercase,setIncludeUppercase] = useState(false);
+  const [includeLowercase,setIncludeLowercase] = useState(false);
+  const [includeNumbers,setIncludeNumbers] = useState(false);
+  const [includeSymbols,setIncludeSymbols] = useState(false);
+
   return (
     <div className="App">
       <div className='container'>
@@ -9,14 +17,16 @@ function App() {
             Password Generator
           </h2>
           <div className='generator__password'>
-            <h3> Password </h3>
+            <h3> {password} </h3>
             <button className='copy__btn'>
               <i className='far fa-clipboard'></i>
             </button>
           </div>
           <div className='form-group'>
-            <label htmlFor='password-strenght'>Password Strenght</label>
+            <label htmlFor='password-strenght'>Password length</label>
             <input
+              defaultValue={passwordlenght}
+              onChange={(e) => setPasswordlenght(e.target.value)}
               type="number"
               id='password-strenght'
               name='password-strenght'
@@ -27,6 +37,8 @@ function App() {
           <div className='form-group'>
             <label htmlFor='uppercase-letters'>Include Uppercase Letters</label>
             <input
+              checked={includeUppercase}
+              onChange={(e) => setIncludeUppercase(e.target.checked)}
               type="checkbox"
               id='uppercase-letters'
               name='uppercase-letters'
@@ -36,6 +48,8 @@ function App() {
           <div className='form-group'>
             <label htmlFor='lowercase-letters'>Include Lowercase Letters</label>
             <input
+              checked={includeLowercase}
+                onChange={(e) => setIncludeLowercase(e.target.checked)}
               type="checkbox"
               id='lowercase-letters'
               name='lowercase-letters'
@@ -45,6 +59,8 @@ function App() {
           <div className='form-group'>
             <label htmlFor='include-numbers'>Include  Numbers</label>
             <input
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
               type="checkbox"
               id='include-numbers'
               name='include-numbers'
@@ -54,6 +70,8 @@ function App() {
           <div className='form-group'>
             <label htmlFor='include-symbols'>Include  Symbols</label>
             <input
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
               type="checkbox"
               id='include-symbols'
               name='include-symbols'
